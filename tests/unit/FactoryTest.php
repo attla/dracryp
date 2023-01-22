058d0823adbfb5d742909c54559e367414cc0628
Nicolau <17990891+nicxlau@users.noreply.github.com>
2023-01-22 17:23:02 -0300

    ⚡ improving tests

--- tests/unit/FactoryTest.php
+++ tests/unit/FactoryTest.php
@@ -3,7 +3,7 @@
 use Attla\Pincryp\Factory as Pincryp;
 
 it(
-    'type is valid',
+    'type is valid [unique]',
     fn ($value) => $this->assertEquals(
         Pincryp::decode(
             Pincryp::encode($value),
@@ -15,7 +15,7 @@ it(
 )->with('var-types');
 
 it(
-    'always different',
+    'always [unique]',
     fn ($value) => $this->assertEquals(count(array_unique(array_map(
         fn() => Pincryp::encode($value),
         range(0, 5)
@@ -23,15 +23,38 @@ it(
 )->with('var-types');
 
 it(
-    'always same',
-    fn ($value) => !Pincryp::setEntropy(0) && $this->assertEquals(count(array_unique(array_map(
+    'decode is invalid [unique]',
+    fn ($value) => $this->assertTrue(
+        Pincryp::decode(
+            Pincryp::encode($value),
+            42,
+            is_array($value) ? true : false
+        ) !== $value
+    )
+)->with('var-types');
+
+it(
+    'type is valid [same]',
+    fn ($value) => !Pincryp::setEntropy(0) && $this->assertEquals(
+        Pincryp::decode(
+            Pincryp::encode($value),
+            '',
+            is_array($value) ? true : false
+        ),
+        $value
+    )
+)->with('var-types');
+
+it(
+    'always [same]',
+    fn ($value) => $this->assertEquals(count(array_unique(array_map(
         fn() => Pincryp::encode($value),
         range(0, 5)
     ))), 1)
 )->with('var-types');
 
 it(
-    'decode is invalid',
+    'decode is invalid [same]',
     fn ($value) => $this->assertTrue(
         Pincryp::decode(
             Pincryp::encode($value),
